1a8235c0d367aefa15ca7d3a20fc89c0d19ea190
DragonWarrior15 <DragonWarrior15@users.noreply.github.com>
2021-06-12 13:41:30 +0530

    ability to train select classes to reduce train time, add logging and folder structure

--- training_script.py
+++ training_script.py
@@ -1,48 +1,96 @@
+import os
+from datetime import datetime
+
+from utils import dogBreedDataset, dogBreedTransforms, dogBreedClassifier, logger_setup
+
 import torch
 from torch.utils.data import DataLoader
 from torch import nn
 
-from utils import dogBreedDataset, dogBreedTransforms, dogBreedClassifier
+log_dir = 'logs'
+model_dir = 'models'
+
+# setup logging and file names
+dt = datetime.now().strftime('%Y%m%d%H%M')
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+logger = logger_setup(os.path.join('logs', f'{dt}'))
+
+logger.info(f'logging to file {os.path.join(log_dir, f"{dt}")}')
+
+# make any directories for saving models
+if not os.path.exists(model_dir):
+    os.mkdir(model_dir)
+if not os.path.exists(os.path.join(model_dir, f'{dt}')):
+    os.mkdir(os.path.join(model_dir, f'{dt}'))
+
+logger.info(f'saving models to folder {os.path.join(model_dir, f"{dt}")}')
+
 
 learning_rate = 1e-3
-batch_size = 64
-epochs = 2
-input_size = 28
-num_classes = 133
+batch_size = 128
+epochs = 100
+input_size = 32
+limit_classes = True
+optimizer_class = torch.optim.Adam
+
+logger.info(f'learning rate is {learning_rate}')
+logger.info(f'batch_size is {batch_size}')
+logger.info(f'epochs is {epochs}')
+logger.info(f'input image size is {input_size}')
+logger.info(f'limit_classes is set to {limit_classes}')
+logger.info(f'optimizer is {optimizer_class}')
 
 transforms = dogBreedTransforms(resize_size=input_size)
 
+logger.info(f'image transforms in use\n {transforms.img_transform}')
+
 training_data = dogBreedDataset('data/dogImages/train',
-                                img_transform=transforms.img_transform)
+                                img_transform=transforms.img_transform,
+                                limit_classes=limit_classes)
 train_dataloader = DataLoader(training_data, batch_size=batch_size,
                                 shuffle=True)
 
+validation_data = dogBreedDataset('data/dogImages/valid',
+                                  img_transform=transforms.img_transform,
+                                  limit_classes=limit_classes)
+validation_dataloader = DataLoader(validation_data, batch_size=len(validation_data),
+                                    shuffle=False)
+
+num_classes = training_data.get_total_classes()
+logger.info(f'total classes are {num_classes}')
+
+# train_dataloader = DataLoader(training_data, batch_size=batch_size,
+                                # sampler=list(range(batch_size)))
+
 model = dogBreedClassifier(input_size=input_size, output_size=num_classes)
 
-print(model)
+logger.info(model)
 
 loss_fn = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = optimizer_class(model.parameters(), lr=learning_rate)
 
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     for batch, (X, y) in enumerate(dataloader):
+        # zero out gradients first
+        optimizer.zero_grad()
+
         # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
 
         # Backpropagation
-        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        # if batch % 100 == 0:
-        if True:
+        if batch % 5 == 0:
+        # if True:
             loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            logger.info(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def test_loop(dataloader, model, loss_fn):
+def val_loop(dataloader, model, loss_fn):
     size = len(dataloader.dataset)
     test_loss, correct = 0, 0
 
@@ -54,10 +102,13 @@ def test_loop(dataloader, model, loss_fn):
 
     test_loss /= size
     correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    logger.info(f"Validation Error: Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
+    logger.info(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
-    # test_loop(test_dataloader, model, loss_fn)
-print("Done!")
+    val_loop(validation_dataloader, model, loss_fn)
+    # save the model
+    torch.save(model.state_dict(), os.path.join(model_dir, f'{dt}', '%05d' % t))
+
+logger.info("Done!")
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import os
+import logging
 
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
-from torchvision.transforms import ToTensor, Resize, Compose, Lambda
+from torchvision import transforms
 from torchvision.io import read_image
 from torch import nn
 
@@ -26,15 +27,24 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class dogBreedTransforms():
     def __init__(self, resize_size=28):
-        self.img_transform = Compose([Resize((resize_size, resize_size)),
-                                      Lambda(lambda x: x/255.0)])
+        self.img_transform = transforms.Compose([
+                                transforms.RandomAffine(degrees=15,
+                                                        translate=(0.1, 0.1),
+                                                        scale=(0.9, 1.1),
+                                                        shear=(-10, 10)),
+                                transforms.RandomHorizontalFlip(p=0.5),
+                                transforms.RandomVerticalFlip(p=0.5),
+                                transforms.Resize((resize_size, resize_size)),
+                                transforms.Lambda(lambda x: x/255.0)
+                             ])
 
 # dataset class
 class dogBreedDataset(Dataset):
     """we will have a map style dataset, need to define init, len and getitem
     the iterable will be a list of tuples where the first item of tuple
     is the class, second is the label, and third is the path of the file"""
-    def __init__(self, img_dir, img_transform=None, target_transform=None):
+    def __init__(self, img_dir, img_transform=None, target_transform=None,
+                 limit_classes=False):
         self.img_dir = img_dir
         self.img_transform = img_transform
         self.target_transform = target_transform
@@ -46,11 +56,16 @@ class dogBreedDataset(Dataset):
         for di in os.listdir(self.img_dir):
             img_class = int(di[:3]) - 1
             img_label = di[4:]
-            if img_class not in self.all_classes:
-                self.all_classes = self.all_classes.union(set([img_class]))
-            for f in os.listdir(os.path.join(self.img_dir, di)):
-                self.img_list.append((img_class, img_label,
-                                        os.path.join(self.img_dir, di, f)))
+
+            # in case limit classes is on, we only keep images for a few classes
+            if(limit_classes and img_class > 9):
+                continue
+            else:
+                if img_class not in self.all_classes:
+                    self.all_classes = self.all_classes.union(set([img_class]))
+                for f in os.listdir(os.path.join(self.img_dir, di)):
+                    self.img_list.append((img_class, img_label,
+                                            os.path.join(self.img_dir, di, f)))
 
     def get_total_classes(self):
         return len(self.all_classes)
@@ -78,18 +93,39 @@ class dogBreedClassifier(nn.Module):
         self.nn_stack = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Flatten(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
             nn.Linear(128, output_size)
         )
 
-    def forward(self, x):
-        # for layer in self.nn_stack:
-            # x = layer(x)
-            # print(x.size())
-        # logits = x
-        logits = self.nn_stack(x)
+    def forward(self, x, debug=False):
+        if(debug):
+            for layer in self.nn_stack:
+                x = layer(x)
+                print(x.size())
+            logits = x
+        else:
+            logits = self.nn_stack(x)
         return logits
+
+def logger_setup(log_file_path):
+    # define logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO) # set the level of logger first
+    # f_handler = logging.StreamHandler('logs')
+    f_handler = logging.FileHandler(log_file_path)
+    f_handler.setLevel(logging.INFO)
+    # f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    f_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    f_handler.setFormatter(f_format)
+    logger.addHandler(f_handler)
+
+    return logger
